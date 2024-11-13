1fb0f2c05b851a6d6f13beb49e3259ebc80c8a42
mahak porwal <porwalmahak784@gmail.com>
2024-11-13 10:52:32 +0530

    first commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from 'next-auth/providers/google';
  const AuthOptions=NextAuth({
-  // Configure one or more authentication providers
+  // Configure one or more authentication providers 
   providers: [
     GithubProvider({
         clientId:process.env.GITHUB_URI,
